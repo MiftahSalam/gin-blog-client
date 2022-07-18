ee9bf4d912e2e77db367b3af735ff9f9fa362941
Miftah Salam <salam.miftah@gmail.com>
2022-07-18 21:31:01 +0700

    add check token expired in method getToken

--- gin-blog-client/src/app/core/services/jwt.service.ts
+++ gin-blog-client/src/app/core/services/jwt.service.ts
@@ -1,13 +1,42 @@
 import { Injectable } from '@angular/core';
+import jwtDecode from 'jwt-decode';
+('jwt-decode');
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class JwtService {
-
-  constructor() { }
+  constructor() {}
 
   getToken(): string {
+    if (window.localStorage.jwtToken) {
+      console.log(
+        'core-services-users-jwtservice-gettoken jwtToken',
+        window.localStorage.jwtToken
+      );
+      const decodedToken: { exp: number; id: number } = jwtDecode(
+        window.localStorage.jwtToken
+      );
+      const expiredDate = new Date(decodedToken.exp * 1000).toLocaleString();
+      const isExpired = Date.now() > decodedToken.exp * 1000;
+      // console.log(
+      //   'core-services-users-jwtservice-gettoken decodedToken',
+      //   decodedToken
+      // );
+      // console.log('core-services-users-jwtservice-gettoken exp', expiredDate);
+      // console.log('core-services-users-jwtservice-gettoken now', new Date());
+      console.log(
+        'core-services-users-jwtservice-gettoken isExpired',
+        isExpired
+      );
+      // console.log(
+      //   'core-services-users-jwtservice-gettoken remain',
+      //   Date.now() - decodedToken.exp * 1000
+      // );
+      if (isExpired) {
+        this.destroyToken();
+      }
+    }
     return window.localStorage.jwtToken;
   }
 
